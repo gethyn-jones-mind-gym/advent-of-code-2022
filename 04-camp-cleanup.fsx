b105ba9eb0c8f9b86c51c143c852c7dee941578a
Gethyn Jones <gethyn.jones@themindgym.com>
2022-12-06 23:03:05 +0000

    Day four ⛺

---
+++ 04-camp-cleanup.fsx
@@ -0,0 +1,60 @@
+open System.IO
+
+exception AssignmentsError of string
+
+let splitAssignments (s: string) =
+  let arr = s.Split(",")
+  match arr.Length with
+  | 2 -> Ok (arr[0], arr[1])
+  | l when l < 2 -> Error "Not enough assignments"
+  | _ -> Error "Too many assignments"
+
+let toSingleDigitSections (shorthand: string) =
+  let arr = shorthand.Split("-")
+  [arr[0] |> int .. arr[1] |> int]
+
+let toSections assignments =
+  assignments
+  |> fun (a1, a2) -> (a1 |> toSingleDigitSections, a2 |> toSingleDigitSections)
+
+let fullyContains a b =
+  a
+  |> List.except b
+  |> fun l -> l.Length = 0
+
+let eitherFullyContains (a, b) =
+  fullyContains a b || fullyContains b a
+
+let implement lines =
+  try
+    lines
+    |> Seq.fold
+      (fun acc line ->
+        line
+        |> splitAssignments
+        |> Result.map toSections
+        |> Result.map eitherFullyContains
+        |> fun b ->
+            match b with
+            | Ok b -> if b = true then acc + 1 else acc
+            | Error ex -> raise (AssignmentsError ex)
+      ) 0
+      |> Ok
+  with
+    | :? AssignmentsError as ex -> Error ex
+
+let readFile path =
+  try
+    File.ReadLines(path)
+    |> Ok
+  with
+  | ex -> Error ex
+
+Path.Combine(__SOURCE_DIRECTORY__, "resources", "camp-cleanup.txt")
+  |> readFile
+  |> Result.map implement
+  |> Result.map
+    (fun result ->
+      match result with
+      | Ok i -> printfn "The total assignments where one range fully contains the other is %A" i
+      | Error ex -> printfn "%A" ex)
